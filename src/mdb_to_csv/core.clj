f274a4b609a9cd4eb2c5b6b990c8f05426b9e3c4
Daniel Abajirov <st155165@stud.uni-stuttgart.de>
2020-07-23 12:46:12 +0200

    Added final function to export a csv with ; separator

--- src/mdb_to_csv/core.clj
+++ src/mdb_to_csv/core.clj
@@ -1,15 +1,18 @@
 (ns mdb-to-csv.core
   (:gen-class)
-  (:require [clojure.walk :as walk]
-            [clojure.data.csv :as csv]
-            [clojure.java.io :as io]
-            [clojess.core :as db])
+  (:require
+   [clojure.walk :as walk]
+   [clojure.data.csv :as csv]
+   [clojure.java.io :as io]
+   [clojess.core :as db])
   (:import
    com.healthmarketscience.jackcess.DatabaseBuilder
    com.healthmarketscience.jackcess.Table
    com.healthmarketscience.jackcess.Table$ColumnOrder
    com.healthmarketscience.jackcess.impl.RowImpl
-   java.io.File))
+   java.io.File
+   java.util.ArrayList
+   java.util.LinkedHashMap))
 
 (defn open-db
   "Opens a database file. Returns a Database object."
@@ -33,30 +36,28 @@
    (map (partial table database)
         (table-names database))))
 
-(defn iteration->seq [iteration]
-  (seq
-   (reify java.lang.Iterable
-     (iterator [this]
-       (reify java.util.Iterator
-         (hasNext [this] (.hasNext iteration))
-         (next [this] (.next iteration))
-         (remove [this] (.remove iteration)))))))
-
-
-(defn rows
-  "Returns a sequence of rows in the table."
+(defn rows-data
+  "Returns a sequence of values-rows in the table that optionally match a pattern {column value}"
   ([table]
    (doall
     (for [row (iterator-seq (.iterator table))]
-      row)))
-  )
+      (.values row)))))
 
+(defn rows-id
+  "Returns a sequence of keys-rows in the table that optionally match a pattern {column value}"
+  ([table]
+   (doall
+    (for [row (iterator-seq (.iterator table))]
+      (.keySet row)))))
 
-
-;(def database-acces (open-db "./resources/BIMWelt.mdb"))
-;; (.setColumnOrder database-acces Table$ColumnOrder/DISPLAY)
-
-
-(def database-acces (atom (open-db "./resources/BIMWelt.mdb")))
-(.setColumnOrder @database-acces Table$ColumnOrder/DISPLAY)
-(def table-access (table @database-acces "Arcs"))
+(defn make-csv [database-path table-name]
+  "Make a csv file based on id and data of selected table"
+  (let [database-acces (atom (open-db database-path))
+        info-display (.setColumnOrder @database-acces Table$ColumnOrder/DISPLAY)
+        table-access (table @database-acces table-name)
+        id-table (into #{} (rows-id table-access))
+        data-table (rows-data table-access)
+        complete-table (concat id-table data-table)]
+    (with-open [writer (io/writer (str table-name ".csv"))]
+      (csv/write-csv writer
+                     complete-table :separator \;))))
---
+++ src/mdb_to_csv/util.clj
@@ -0,0 +1,66 @@
+(ns mdb-to-csv.util
+  (:require [clojure.string :as s]
+            [clojure.walk :as walk]))
+
+(defn camelcase-to-dashes
+  [string]
+  (let [parts (partition-by #(Character/isUpperCase %) string)]
+    (loop [parts parts, acc []]
+      (if (seq parts)
+        (if (Character/isUpperCase (ffirst parts))
+          (let [part (s/join (concat (first parts) (second parts)))]
+            (recur (nnext parts) (conj acc (s/lower-case part))))
+          (recur (next parts) (conj acc (s/lower-case (s/join (first parts))))))
+        (s/join "-" acc)))))
+
+(defn keywordize-keys
+  [m]
+  (let [f (fn [[k v]]
+            (if (string? k)
+              [(keyword (camelcase-to-dashes k)) v]
+              [k v]))]
+    (walk/postwalk (fn [x]
+                     (if (map? x)
+                       (into {} (map f x))
+                       x))
+                   m)))
+
+(defn columns-of-index
+  [index]
+  (for [column (.getColumns index)] (.getName column)))
+
+(defn index-has-columns?
+  [columns index]
+  (= (map name columns) (columns-of-index index)))
+
+(defn index-for-columns
+  [table columns]
+  (first
+   (for [index (.getIndexes table)
+         :when (index-has-columns? columns index)]
+     index)))
+
+(defn matching-rows-from-cursor
+  [cursor pattern]
+  (doall
+   (let [pattern (walk/stringify-keys pattern)]
+     (for [row (iterator-seq
+                (reify java.util.Iterator
+                  (hasNext [this]
+                    (let [savepoint (.getSavepoint cursor)
+                          result (.findNextRow cursor pattern)]
+                      (.restoreSavepoint cursor savepoint)
+                      result))
+                  (next [this]
+                    (.findNextRow cursor pattern)
+                    (.getCurrentRow cursor))
+                  (remove [this]
+                    (.deleteCurrentRow cursor))))]
+       (walk/keywordize-keys (into {} row))))))
+
+(defn matching-rows
+  [table pattern]
+  (let [columns (keys pattern)
+        cursor (or (some-> (index-for-columns table columns) .newCursor .toCursor)
+                   (-> table .newCursor .toCursor))]
+    (matching-rows-from-cursor cursor pattern)))
